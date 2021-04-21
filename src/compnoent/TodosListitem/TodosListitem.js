12bf489503703858eb9b4b64ff3b4047dc294467
zivi5645 <zivi5645@gmail.com>
2021-04-21 18:53:11 +0300

    delete todo

--- src/compnoent/TodosListitem/TodosListitem.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from "react";
-
-export class TodosListitem extends Component {
-  render() {
-    return <div>{this.props.titel}</div>;
-  }
-}
-
-export default TodosListitem;
