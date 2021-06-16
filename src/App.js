67ee7ad9f94f4b4ed4a0db09500295b9da0d605c
zivi5645 <zivi5645@gmail.com>
2021-06-16 16:25:39 +0300

    Color replacement

--- src/App.js
+++ src/App.js
@@ -1,34 +1,36 @@
-import React, { useEffect, useState } from "react";
-// import "./App.css";
+import React, { useState } from "react";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import Home from "./pages/Home/Home";
 import About from "./pages/About/About";
+import ThemeContext, { themes } from "./ThemeContext";
 
 const App = () => {
+  const [currntTheme, setCurrntTheme] = useState(themes.light);
   return (
-    <Router>
-      <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/about">About</Link>
-            </li>
-          </ul>
-        </nav>
+    <ThemeContext.Provider value={currntTheme}>
+      <button onClick={() => setCurrntTheme(themes.dark)}>Change theme</button>
+      <Router>
+        <div>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/about">About</Link>
+              </li>
+            </ul>
+          </nav>
 
-        {/* A <Switch> looks through its children <Route>s and
-          renders the first one that matches the current URL. */}
-        <Switch>
-          <Route path="/products/:productId" component={About}></Route>
-          <Route path="/">
-            <Home />
-          </Route>
-        </Switch>
-      </div>
-    </Router>
+          <Switch>
+            <Route path="/products/:productId" component={About}></Route>
+            <Route path="/">
+              <Home />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
+    </ThemeContext.Provider>
   );
 };
 
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,12 +1,17 @@
-import React, { useEffect, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import "./Home.css";
 // import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import Header from "../../compnoent/Header/Header";
 import Products from "../../compnoent/Products/Products";
+import RangeSlider from "../../compnoent/PriceRange/PriceRange";
+import ThemeContext from "../../ThemeContext";
 
 const Home = () => {
   const [products, setProducts] = useState([]);
   const [selectedCategory, setselectedCategory] = useState("");
+  const [filteredPrice, setFilteredPrice] = useState();
+  const theme = useContext(ThemeContext);
+  // console.log("theme", theme);
 
   const groupBy = (xs, key) =>
     xs.reduce((rv, x) => {
@@ -15,7 +20,7 @@ const Home = () => {
     }, {});
 
   useEffect(() => {
-    fetch("https://fakestoreapi.com/products")
+    fetch("http://localhost:8000/products")
       .then((response) => response.json())
       .then((products) => setProducts(products));
   }, []);
@@ -24,8 +29,35 @@ const Home = () => {
     (product) =>
       product.category === selectedCategory || selectedCategory === ""
   );
+  // useEffect(() => {
+  //   const getProducts = async () => {
+  //     const response = await fetch("http://localhost:8000/products", {
+  //       method: "GET",
+  //     });
+  //     let productsFromServer = await response.json();
+  //     setProducts(productsFromServer);
+  //   };
+  //   getProducts();
+  // });
+  // let filteredProducts = products.filter(
+  //   (product) =>
+  //     product.category === selectedCategory || selectedCategory === ""
+  // );
+
+  // const MaxMin = () => {
+  //     const priceArray=
+  //   products.reduce((price, cur) => {
+  //     return(
+  //     price.min > cur ? price.min : cur
+  //         price.max < cur ? price.max : cur
+  //         )
+  //   }, {max[0], min[1]});
+  // };
+  console.log("filteredPrice", filteredPrice);
+  const setFunc = (event, value) => setFilteredPrice(value);
   return (
-    <div className="app">
+    <div className="app" style={{ background: theme.background }}>
+      <RangeSlider setFilteredPrice={setFunc}></RangeSlider>
       <Header
         category={selectedCategory}
         setselectedCategory={setselectedCategory}
